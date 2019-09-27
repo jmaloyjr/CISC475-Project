8d69bf0461407bfaa9bc1bc9f1b27385b85ffc49
Cong Meng <samuelmc1015@gmail.com>
2019-09-26 22:08:45 -0400

    remove .idea and venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\samue\Desktop\Python Workspace\Fullstack Development\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
