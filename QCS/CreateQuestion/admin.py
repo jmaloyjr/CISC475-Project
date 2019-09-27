a4ebc66bad4414f1f4f49728ae656c1a5a06cdf8
Cong Meng <samuelmc1015@gmail.com>
2019-09-26 22:21:06 -0400

    databse prototype

--- QCS/CreateQuestion/admin.py
+++ QCS/CreateQuestion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+myModels = [Question, Tag, Course]
+admin.site.register(myModels)
--- QCS/CreateQuestion/models.py
+++ QCS/CreateQuestion/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+# import datetime
+
+
+class Tag(models.Model):
+    tag_text = models.CharField(max_length=200)
+
+
+class Course(models.Model):
+    course_text = models.CharField(max_length=200)
 
 
 class Question(models.Model):
+    question_name = models.CharField(max_length=200)
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    create_date = models.DateTimeField()
+    modified_date = models.DateTimeField()
+    version = models.IntegerField(default=1)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
